1feea5e606ebd9caa5ec9ac101441fab98d0e88e
Caroline Tait <cjtaituk@yahoo.co.uk>
2020-01-09 11:17:08 +0000

    update files

--- R/get_cid.R
+++ R/get_cid.R
@@ -7,7 +7,7 @@
 #' @export
 #'
 #' @examples
-#' get_cid_line(type = "advanced_stop_line")
+#' # get_cid_line(type = "advanced_stop_line")
 get_cid_line = function(type) {
   base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/lines/"
   cid_url = paste0(base_url, type, ".json")
