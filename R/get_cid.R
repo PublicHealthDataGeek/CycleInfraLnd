af34f6c0a500e2b1f0beb4b0245d1b57badb3744
Caroline Tait <cjtaituk@yahoo.co.uk>
2020-01-09 10:46:37 +0000

    add get_cid function

---
+++ R/get_cid.R
@@ -0,0 +1,14 @@
+#' Get Cycling Infrastructure data on lines
+#'
+#' @param type Type of infrastructure
+#'
+#' @return sf object
+#'
+#' @export
+#'
+#' @examples
+get_cid_line = function(type) {
+  base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/lines/"
+  cid_url = paste0(base_url, type, ".json")
+  sf::read_sf(cid_url)
+}
