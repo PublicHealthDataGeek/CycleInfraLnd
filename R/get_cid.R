938c65da41af42fab6efd7228a0bef0beac44664
Caroline Tait <cjtaituk@yahoo.co.uk>
2020-01-09 11:23:31 +0000

    created new function for points

--- R/get_cid.R
+++ R/get_cid.R
@@ -7,9 +7,25 @@
 #' @export
 #'
 #' @examples
-#' # get_cid_line(type = "advanced_stop_line")
-get_cid_line = function(type) {
+#' # get_cid_lines(type = "advanced_stop_line")
+get_cid_lines = function(type) {
   base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/lines/"
+  cid_url = paste0(base_url, type, ".json")
+  sf::read_sf(cid_url)
+}
+
+#' Get Cycling Infrastructure data on points
+#'
+#' @param type Type of infrastructure
+#'
+#' @return sf object
+#'
+#' @export
+#'
+#' @examples
+#' # get_cid_points(type = "signal")
+get_cid_points = function(type) {
+  base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/points/"
   cid_url = paste0(base_url, type, ".json")
   sf::read_sf(cid_url)
 }
