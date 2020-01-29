9aa34455dc61df571cd0e1d419d461ca7746d503
Caroline Tait <cjtaituk@yahoo.co.uk>
2020-01-29 12:47:56 +0000

    all examples included

--- R/get_cid.R
+++ R/get_cid.R
@@ -7,7 +7,10 @@
 #' @export
 #'
 #' @examples
-#' # get_cid_lines(type = "advanced_stop_line")
+#' get_cid_lines(type = "advanced_stop_line")
+#' get_cid_lines(type = "crossing")
+#' get_cid_lines(type = "cycle_lane_track")
+#' get_cid_lines(type = "restricted_route")
 get_cid_lines = function(type) {
   base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/lines/"
   cid_url = paste0(base_url, type, ".json")
@@ -23,7 +26,11 @@ get_cid_lines = function(type) {
 #' @export
 #'
 #' @examples
-#' # get_cid_points(type = "signal")
+#' get_cid_points(type = "signal")
+#' get_cid_points(type = "cycle_parking")
+#' get_cid_points(type = "restricted_point")
+#' get_cid_points(type = "signage")
+#' get_cid_points(type = "traffic_calming")
 get_cid_points = function(type) {
   base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/points/"
   cid_url = paste0(base_url, type, ".json")
